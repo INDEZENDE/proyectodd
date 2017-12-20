1674c6398ca5904c14f381b7a3fa79bb5f0f578c
glous09 <goyavaes@hotmail.es>
2017-12-20 15:35:33 -0600

    Login html y loguin js

--- js/login.js
+++ js/login.js
@@ -1,9 +1,11 @@
-/function valida_login(){
+function valida_login(){
   //Validamos un campo o área de texto, por ejemplo el campo usuario
 
+
   if (document.form.username.value=='Lucile' && document.form.password.value=='123'){
-         document.form.submit("index.html");
-        /*location="index.html"*/
+         /*document.form.submit("index.html");*/
+         window.location="../index.html"
+      /*window.location="../views/aqp2016-2.html";*/
       }
       else{
            alert("Por favor ingrese, nombre de usuario y contraseña correctos.");
